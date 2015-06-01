906052b13026f55a7176d183d6350ce7ce59186f
darrenmc <darren@kurralta.com>
2015-06-01 01:08:22 +0100

    Simplify date provider

--- src/main/scala/com/abc/DateProvider.scala
+++ src/main/scala/com/abc/DateProvider.scala
@@ -4,15 +4,6 @@ import java.util.Calendar
 import java.util.Date
 
 object DateProvider {
-  def getInstance: DateProvider = {
-    if (instance == null) instance = new DateProvider
-    instance
-  }
-
-  private var instance: DateProvider = null
-}
-
-class DateProvider {
   def now: Date = Calendar.getInstance.getTime
 }
 
