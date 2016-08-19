128513aa819c65d14afed26fb115b4a860c386aa
sark0 <ksark1991@gmail.com>
2016-08-19 15:32:33 -0400

    Update Account.scala

--- src/main/scala/com/abc/Account.scala
+++ src/main/scala/com/abc/Account.scala
@@ -1,6 +1,7 @@
 package com.abc
 
 import scala.collection.mutable.ListBuffer
+import java.util.Date
 
 object Account {
   final val CHECKING: Int = 0
@@ -14,31 +15,31 @@ class Account(val accountType: Int, var transactions: ListBuffer[Transaction] = 
     if (amount <= 0)
       throw new IllegalArgumentException("amount must be greater than zero")
     else
-      transactions += Transaction(amount)
+      transactions += new Transaction(amount)
   }
 
   def withdraw(amount: Double) {
     if (amount <= 0)
       throw new IllegalArgumentException("amount must be greater than zero")
     else
-      transactions += Transaction(-amount)
+      transactions += new Transaction(-amount)
   }
 
   def interestEarned: Double = {
-    val amount: Double = sumTransactions()
+    val amount: Double = sumTransactions() + accruedInterest
     accountType match {
       case Account.SAVINGS =>
-        if (amount <= 1000) amount * 0.001
-        else 1 + (amount - 1000) * 0.002
+        if (amount <= 1000) amount * 0.001/365
+        else 1/365 + (amount - 1000) * 0.002/365
       case Account.MAXI_SAVINGS =>
-        if (amount <= 1000) return amount * 0.02
-        if (amount <= 2000) return 20 + (amount - 1000) * 0.05
-        70 + (amount - 2000) * 0.1
+        if (amount <= 1000) return amount * 0.02/365
+        if (amount <= 2000) return 20 + (amount - 1000) * 0.05/365
+        70 + (amount - 2000) * 0.1/365
       case _ =>
         amount * 0.001
     }
   }
 
   def sumTransactions(checkAllTransactions: Boolean = true): Double = transactions.map(_.amount).sum
-
+  def accumulateInterest = accruedInterest +=  interestEarned
 }
